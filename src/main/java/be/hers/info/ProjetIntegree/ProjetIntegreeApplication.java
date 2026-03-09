4ea7b373f041576bc5de51361ade92fd5b639167
Vatafu Jean <jean.vatafu@student.hers.be>
2026-03-09 20:14:25 +0100

    feat: Fichier Spring Web

---
+++ src/main/java/be/hers/info/ProjetIntegree/ProjetIntegreeApplication.java
@@ -0,0 +1,13 @@
+package be.hers.info.ProjetIntegree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntegreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIntegreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/hers/info/ProjetIntegree/ProjetIntegreeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hers.info.ProjetIntegree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
