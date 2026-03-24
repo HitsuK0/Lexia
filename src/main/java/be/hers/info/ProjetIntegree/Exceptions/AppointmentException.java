dd66ad1448d8ad64eb8dc2bb74f7e4b91c7f1180
Vatafu Jean <jean.vatafu@student.hers.be>
2026-03-24 18:51:12 +0100

    feat: ajout de la classe AppointementException

---
+++ src/main/java/be/hers/info/ProjetIntegree/Exceptions/AppointmentException.java
@@ -0,0 +1,7 @@
+package be.hers.info.ProjetIntegree.Exceptions;
+
+public class AppointmentException extends Exception {
+    public AppointmentException() {}
+
+    public AppointmentException(String m) {super(m);}
+}
--- src/main/java/be/hers/info/ProjetIntegree/POJO/Appointment.java
+++ src/main/java/be/hers/info/ProjetIntegree/POJO/Appointment.java
@@ -1,129 +1,74 @@
 package be.hers.info.ProjetIntegree.POJO;
 
+import be.hers.info.ProjetIntegree.Exceptions.AppointmentException;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Appointment {
     private int appointment_ID;
-    private LocalTime appointement_start_time;
-    private LocalTime appointement_end_time;
-    private AppointmentStatus status;
-    private List<String> appointement_locals;
+    private String status;
+    private List<String> appointement_locals; // TODO : supression ?
     private Beneficiary beneficiary;
     private List<Interpreter> interpreters;
     private TimeSlotPunctual time_slot_punctual;
-    private List<academicSkill> academic_skills_needed;
-    private List<businessSkill> business_skills_needed;
-
-    // TODO : Replace all exceptions by personalised ones
-
+    private TimeSlotBase time_slot_base;
+    private List<AcademicSkill> academic_skills_needed;
+    private List<BusinessSkill> business_skills_needed;
 
     public Appointment() {}
 
-    public Appointment(Beneficiary beneficiary, LocalTime appointementStartTime, LocalTime appointementEndTime) {
+    public Appointment(Beneficiary beneficiary) {
         this.beneficiary = beneficiary;
-        this.appointement_start_time = appointementStartTime;
-        this.appointement_end_time = appointementEndTime;
-        this.status = AppointmentStatus.EN_ATTENTE;
+        this.status = "En attente";
         this.appointement_locals = new ArrayList<String>();
         this.interpreters = new ArrayList<Interpreter>();
-        this.academic_skills_needed = new ArrayList<academicSkill>();
-        this.business_skills_needed = new ArrayList<businessSkill>();
+        this.academic_skills_needed = new ArrayList<AcademicSkill>();
+        this.business_skills_needed = new ArrayList<BusinessSkill>();
     }
 
-    public int getAppointmentID() {
+    public int get_appointment_ID() {
         return appointment_ID;
     }
 
-    public LocalTime getAppointementStartTime() {
-        return appointement_start_time;
-    }
-
-    public LocalTime getAppointementEndTime() {
-        return appointement_end_time;
-    }
-
-    public AppointmentStatus getStatus() {
+    public String get_status() {
         return status;
     }
 
-    public List<String> getAppointementLocals() {
+    public List<String> get_appointement_locals() {
         return appointement_locals;
     }
 
-    public Beneficiary getBeneficiary() {
+    public Beneficiary get_beneficiary() {
         return beneficiary;
     }
 
-    public List<Interpreter> getInterpreters() {
-        return interpreters;
-    }
+    public List<Interpreter> get_interpreters() { return interpreters;}
 
-    public TimeSlotPunctual getTimeSlotPunctual() {
-        return time_slot_punctual;
-    }
+    public TimeSlotPunctual get_time_slot_punctual() { return time_slot_punctual;}
 
-    public void setStatus(AppointmentStatus status) throws IllegalArgumentException { // TODO : add throws for unwanted status setter, only {Accepte, Refuse, En attente}
-        if(this.status == AppointmentStatus.ACCEPTE || this.status == AppointmentStatus.REFUSE) {
-            throw new IllegalArgumentException("Le status ne peut plus etre modifie.");
+    public TimeSlotBase get_time_slot_base() { return time_slot_base; }
+
+    public void set_status(String status) throws AppointmentException {
+        if(this.status.equals("Accepte") || this.status.equals("Refuse")) {
+            throw new AppointmentException("Le status ne peut plus etre modifie.");
         }
 
-        if(this.status == status) {
-            throw new IllegalArgumentException("Le status de ce rendez-vous est deja " + status); // TODO : att toString to status for lower case?
+        if(this.status.equals(status)) {
+            throw new AppointmentException("Le status de ce rendez-vous est deja " + status);
         }
 
-        this.status = status; // TODO : PAS COMPLET
+        this.status = status;
     }
 
-    public void addLocal(String l) throws IllegalArgumentException {
-        if(appointement_locals.contains(l)) {
-            throw new IllegalArgumentException("Ce local a deja ete ajoute a ce rendez-vous.");
-        }
+    public void set_appointement_start_time(LocalTime sT) {this.appointement_start_time = sT;}
 
-        appointement_locals.add(l);
-    }
-
-    public void removeLocal(String l) throws IllegalArgumentException {
-        if(!appointement_locals.contains(l)) {
-            throw new IllegalArgumentException("Ce local ne fait pas partie de ce rendez-vous.");
-        }
-
-        appointement_locals.remove(l);
-    }
-
-    public void addInterpreter(Interpreter i) {
-        // TODO : i need Interpreter class to do this
-    }
-
-    public void removeInterpreter(Interpreter i) {
-        // TODO : same here
-    }
-
-    public void addAcademicSkills(academicSkill a) {
-        // TODO : i need academicSkills class to do this
-    }
-
-    public void removeAcademicSkill(academicSkill a) {
-        // TODO : i need academicSkills class to do this
-    }
-
-    public void addBusinessSkill(businessSkill b) {
-        // TODO : i need academicSkills class to do this
-    }
-
-    public void removeBusinessSkill(businessSkill b) {
-        // TODO : i need academicSkills class to do this
-    }
-
-
-    // TODO : maybe incomplete ?
-    public void setAppointementStartTime(LocalTime sT) {
-        this.appointement_start_time = sT;
-    }
-
-    public void setAppointementEndTime(LocalTime eT) {
+    public void set_sppointement_end_time(LocalTime eT) {
         this.appointement_end_time = eT;
+    }
+
+    public String toString() {
+        return "Appointment {}";
     }
 }
