30578473dbf01828cef62862450c444923fd83ac
Vatafu Jean <jean.vatafu@student.hers.be>
2026-03-25 14:05:34 +0100

    fix: corrections, lancement des exceptions quand certains attributs null

--- src/main/java/be/hers/info/ProjetIntegree/POJO/Appointment.java
+++ src/main/java/be/hers/info/ProjetIntegree/POJO/Appointment.java
@@ -7,9 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Appointment {
-    private int appointment_ID;
     private String status;
-    private List<String> appointement_locals; // TODO : supression ?
+    private List<String> appointement_locals;
     private Beneficiary beneficiary;
     private List<Interpreter> interpreters;
     private TimeSlotPunctual time_slot_punctual;
@@ -17,38 +16,159 @@ public class Appointment {
     private List<AcademicSkill> academic_skills_needed;
     private List<BusinessSkill> business_skills_needed;
 
-    public Appointment() {}
+    public Appointment(Beneficiary beneficiary, List<String> appointement_locals, List<Interpreter> interpreters, List<AcademicSkill> academic_skills_needed, List<BusinessSkill> business_skills_needed,
+                       TimeSlotPunctual time_slot_punctual, TimeSlotBase time_slot_base) {
+        if(beneficiary == null || appointement_locals == null || interpreters == null || academic_skills_needed == null || business_skills_needed == null || time_slot_punctual == null || time_slot_base == null) {
+            throw new NullPointerException();
+        }
 
-    public Appointment(Beneficiary beneficiary) {
+        if(appointement_locals.size() == 0 || interpreters.size() == 0 || academic_skills_needed.size() == 0 || business_skills_needed.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
         this.beneficiary = beneficiary;
         this.status = "En attente";
-        this.appointement_locals = new ArrayList<String>();
-        this.interpreters = new ArrayList<Interpreter>();
-        this.academic_skills_needed = new ArrayList<AcademicSkill>();
-        this.business_skills_needed = new ArrayList<BusinessSkill>();
+        this.appointement_locals = appointement_locals;
+        this.interpreters = interpreters;
+        this.time_slot_punctual = time_slot_punctual;
+        this.time_slot_base = time_slot_base;
+        this.academic_skills_needed = academic_skills_needed;
+        this.business_skills_needed = business_skills_needed;
     }
 
-    public int get_appointment_ID() {
-        return appointment_ID;
+    public Appointment() {
+        status = "En attente";
+        appointement_locals = new ArrayList<String>();
+        interpreters = null;
+        time_slot_punctual = null;
+        time_slot_base = null;
+        academic_skills_needed = new ArrayList<AcademicSkill>();
+        business_skills_needed = new ArrayList<BusinessSkill>();
     }
 
-    public String get_status() {
+    public String getStatus() {
         return status;
     }
 
-    public List<String> get_appointement_locals() {
+    public List<String> getAppointement_locals() {
         return appointement_locals;
     }
 
-    public Beneficiary get_beneficiary() {
+    public Beneficiary getBeneficiary() {
         return beneficiary;
     }
 
-    public List<Interpreter> get_interpreters() { return interpreters;}
+    public List<Interpreter> getInterpreters() {
+        return interpreters;
+    }
 
-    public TimeSlotPunctual get_time_slot_punctual() { return time_slot_punctual;}
+    public TimeSlotPunctual getTime_slot_punctual() {
+        return time_slot_punctual;
+    }
 
-    public TimeSlotBase get_time_slot_base() { return time_slot_base; }
+    public TimeSlotBase getTime_slot_base() {
+        return time_slot_base;
+    }
+
+    public List<AcademicSkill> getAcademic_skills_needed() {
+        return academic_skills_needed;
+    }
+
+    public List<BusinessSkill> getBusiness_skills_needed() {
+        return business_skills_needed;
+    }
+
+    public void set_appointement_start_time(LocalTime sT) {
+        if(sT == null) {
+            throw new NullPointerException();
+        }
+
+        this.appointement_start_time = sT;
+    }
+
+    public void set_sppointement_end_time(LocalTime eT) {
+        if(eT == null) {
+            throw new NullPointerException();
+        }
+
+        this.appointement_end_time = eT;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAppointement_locals(List<String> appointement_locals) {
+        if(appointement_locals == null) {
+            throw new NullPointerException();
+        }
+
+        if(appointement_locals.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.appointement_locals = appointement_locals;
+    }
+
+    public void setBeneficiary(Beneficiary beneficiary) {
+        if(beneficiary == null) {
+            throw new NullPointerException();
+        }
+
+        this.beneficiary = beneficiary;
+    }
+
+    public void setInterpreters(List<Interpreter> interpreters) {
+        if(interpreters == null) {
+            throw new NullPointerException();
+        }
+
+        if(interpreters.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.interpreters = interpreters;
+    }
+
+    public void setTime_slot_punctual(TimeSlotPunctual time_slot_punctual) {
+        if(time_slot_punctual == null) {
+            throw new NullPointerException();
+        }
+
+        this.time_slot_punctual = time_slot_punctual;
+    }
+
+    public void setTime_slot_base(TimeSlotBase time_slot_base) {
+        if(time_slot_base == null) {
+            throw new NullPointerException();
+        }
+
+        this.time_slot_base = time_slot_base;
+    }
+
+    public void setAcademic_skills_needed(List<AcademicSkill> academic_skills_needed) {
+        if(academic_skills_needed == null) {
+            throw new NullPointerException();
+        }
+
+        if(academic_skills_needed.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.academic_skills_needed = academic_skills_needed;
+    }
+
+    public void setBusiness_skills_needed(List<BusinessSkill> business_skills_needed) {
+        if(business_skills_needed == null) {
+            throw new NullPointerException();
+        }
+
+        if(business_skills_needed.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.business_skills_needed = business_skills_needed;
+    }
 
     public void set_status(String status) throws AppointmentException {
         if(this.status.equals("Accepte") || this.status.equals("Refuse")) {
@@ -60,12 +180,6 @@ public class Appointment {
         }
 
         this.status = status;
-    }
-
-    public void set_appointement_start_time(LocalTime sT) {this.appointement_start_time = sT;}
-
-    public void set_sppointement_end_time(LocalTime eT) {
-        this.appointement_end_time = eT;
     }
 
     public String toString() {
