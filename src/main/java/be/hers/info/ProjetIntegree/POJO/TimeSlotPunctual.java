a22dd334e759225ed804ad9e49bf2fe11a13976e
Vatafu Jean <jean.vatafu@student.hers.be>
2026-03-28 08:50:36 +0100

    feat: Ajout des setters pour les attributs de TimeSlotPunctual

--- src/main/java/be/hers/info/ProjetIntegree/POJO/TimeSlotPunctual.java
+++ src/main/java/be/hers/info/ProjetIntegree/POJO/TimeSlotPunctual.java
@@ -1,31 +1,108 @@
 package be.hers.info.ProjetIntegree.POJO;
 
+import org.springframework.cglib.core.Local;
+
+import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class TimeSlotPunctual extends TimeSlot {
-    private final LocalDate startDate;
-    private final LocalDate endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
+    /**
+     * Initialize a TimeSlotPunctual with startTime, duration, startDate and endDate is null by default
+     * @param startTime the start time of the time slot
+     * @param duration the duration
+     * @param startDate the concerned date
+     * @throws NullPointerException if startDate is null
+     */
     public TimeSlotPunctual(LocalTime startTime, LocalTime duration, LocalDate startDate) {
         super(startTime, duration);
+
+        if(startDate == null) {
+            throw new NullPointerException();
+        }
+
         this.startDate = startDate;
-        endDate = null;
+        this.endDate = null;
     }
 
+    /**
+     * Initialize a TimeSlotPunctual with startTime, duration, startDate and endDate
+     * @param startTime the start time of the time slot
+     * @param duration the duration
+     * @param startDate the start date of the time slot
+     * @param endDate the end date of the time slot
+     * @throws NullPointerException if startDate or endDate is null
+     */
     public TimeSlotPunctual(LocalTime startTime, LocalTime duration, LocalDate startDate, LocalDate endDate) {
         super(startTime, duration);
+
+        if(startDate == null || endDate == null) {
+            throw new NullPointerException();
+        }
+
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
+    /**
+     * Initialize a TimeSlotPunctual with no elements
+     */
+    public TimeSlotPunctual() {
+        super(null, null);
+        this.startDate = null;
+        this.endDate = null;
+    }
+
+    /**
+     * @return the start date
+     */
     public LocalDate getStartDate() {
 
         return startDate;
     }
 
+    /**
+     * @return the end date
+     */
     public LocalDate getEndDate() {
         return endDate;
+    }
+
+    /**
+     * @param startDate the start date to set
+     * @throws NullPointerException if startDate is null
+     * @throws IllegalArgumentException if this.startDate is not null and this.startDate is after startDate
+     */
+    public void setStartDate(LocalDate startDate) {
+        if(startDate ==  null) {
+            throw new NullPointerException();
+        }
+
+        if(this.startDate != null && this.startDate.isAfter(startDate)) {
+            throw new IllegalArgumentException();
+        }
+
+        this.startDate = startDate;
+    }
+
+    /**
+     * @param endDate the end date to set
+     * @throws NullPointerException if endDate is null
+     * @throws IllegalArgumentException if this.startDate is not null and endDate is before this.startDate
+     */
+    public void setEndDate(LocalDate endDate) {
+        if(endDate == null) {
+            throw new NullPointerException();
+        }
+
+        if(this.startDate != null && endDate.isBefore(this.startDate)) {
+            throw new IllegalArgumentException();
+        }
+
+        this.endDate = endDate;
     }
 
     @Override
