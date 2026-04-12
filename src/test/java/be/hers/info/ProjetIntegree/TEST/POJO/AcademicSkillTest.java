5eb006f78511872434af38ea0b566e6475055d59
Jean-Francois Nicolas <jf.nicolas@student.hers.be>
2026-04-12 11:42:31 +0200

    feat: Ajout de la méthode setUp et du premier test constucteur -> ConstructorWithValidDesignation

--- src/test/java/be/hers/info/ProjetIntegree/TEST/POJO/AcademicSkillTest.java
+++ src/test/java/be/hers/info/ProjetIntegree/TEST/POJO/AcademicSkillTest.java
@@ -1,4 +1,31 @@
 package be.hers.info.ProjetIntegree.TEST.POJO;
 
+import be.hers.info.ProjetIntegree.POJO.AcademicSkill;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Nicolas Jean-François
+ * @reviewer Halet Louis
+ */
+
 public class AcademicSkillTest {
+    private AcademicSkill skill;
+
+    @BeforeEach
+    void setUp(){
+        skill = new AcademicSkill("Mathématiques");
+    }
+
+    // Constructor with parameter //
+
+    @Test
+    void constructorWithValidDesignation() {
+        AcademicSkill a = new AcademicSkill("Informatique");
+        assertEquals("Informatique", a.getDesignation());
+    }
+
+    
 }
