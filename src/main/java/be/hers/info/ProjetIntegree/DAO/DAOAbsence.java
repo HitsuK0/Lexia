710d5359724f15cc723151ee6f23d858b5dbf659
Vatafu Jean <jean.vatafu@student.hers.be>
2026-04-12 11:15:35 +0200

    feat: Ajout de la fonction find

---
+++ src/main/java/be/hers/info/ProjetIntegree/DAO/DAOAbsence.java
@@ -0,0 +1,76 @@
+package be.hers.info.ProjetIntegree.DAO;
+
+import be.hers.info.ProjetIntegree.POJO.Absence;
+import be.hers.info.ProjetIntegree.POJO.Interpreter;
+import be.hers.info.ProjetIntegree.POJO.TimeSlotPunctual;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DAOAbsence extends DAO<Absence> {
+
+    @Override
+    public Absence find(int objectToSearchInDB) throws SQLException {
+        Absence absence = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        String query = "SELECT status, FKTimeSlotPunctual " +
+                "FROM Absence " +
+                "WHERE numAbsence = ?";
+
+        try {
+            preparedStatement = connect.prepareStatement(query);
+            preparedStatement.setInt(1, objectToSearchInDB);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                DAOTimeSlotPunctual daoTimeSlotPunctual = new DAOTimeSlotPunctual();
+                TimeSlotPunctual timeSlotPunctual = daoTimeSlotPunctual.find(resultSet.getInt("FKTimeSlotPunctual"));
+
+                absence = new Absence(objectToSearchInDB, resultSet.getString("status"),timeSlotPunctual);
+            }
+        } finally {
+            if(resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return absence;
+        }
+    }
+
+    @Override
+    public List<Absence> findAll() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public boolean create(Absence objectToInsertInDB) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Absence objectToUpdateInDB) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Absence objectToDeleteFormDB) throws SQLException {
+        return false;
+    }
+}
