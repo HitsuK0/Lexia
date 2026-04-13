0a41884f15c2422471dfd54d9f78eb228abdb07b
Vatafu Jean <jean.vatafu@student.hers.be>
2026-04-13 17:21:10 +0200

    refactor: ajustements

--- src/main/java/be/hers/info/ProjetIntegree/DAO/DAOAbsence.java
+++ src/main/java/be/hers/info/ProjetIntegree/DAO/DAOAbsence.java
@@ -1,19 +1,30 @@
 package be.hers.info.ProjetIntegree.DAO;
 
 import be.hers.info.ProjetIntegree.POJO.Absence;
+import be.hers.info.ProjetIntegree.POJO.BadStatusException;
 import be.hers.info.ProjetIntegree.POJO.Interpreter;
 import be.hers.info.ProjetIntegree.POJO.TimeSlotPunctual;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DAOAbsence extends DAO<Absence> {
+/**
+ * @author Vatafu Jean
+ * @reviewer
+ */
 
-    @Override
-    public Absence find(int objectToSearchInDB) throws SQLException {
+public class DAOAbsence {
+
+    /**
+     *
+     * @throws BadStatusException if the absence found has an invalid status
+     */
+    public Absence find(int objectToSearchInDB) throws SQLException, BadStatusException {
+        Connection connect = ConnectionOracle.getInstance();
         Absence absence = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -50,13 +61,16 @@ public class DAOAbsence extends DAO<Absence> {
                     e.printStackTrace();
                 }
             }
-
-            return absence;
         }
+        return absence;
     }
 
-    @Override
-    public List<Absence> findAll() throws SQLException {
+    /**
+     *
+     * @throws BadStatusException if the absence found has an invalid status
+     */
+    public List<Absence> findAll() throws SQLException, BadStatusException {
+        Connection connect = ConnectionOracle.getInstance();
         List<Absence> absenceList = new ArrayList<Absence>();
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -92,30 +106,36 @@ public class DAOAbsence extends DAO<Absence> {
                     e.printStackTrace();
                 }
             }
-
-            return absenceList;
         }
+        return absenceList;
     }
 
-    @Override
-    public boolean create(Absence objectToInsertInDB) throws SQLException {
-        boolean isCreated = false;
+    /**
+     *
+     * @param otherObjectID The id of the Interpreter that this absence refers to
+     * @throws IllegalArgumentException If otherObjectID is negative
+     */
+    public boolean create(Absence objectToInsertInDB, int otherObjectID) throws SQLException, IllegalArgumentException {
+        Connection connect = ConnectionOracle.getInstance();
+        boolean created = false;
         PreparedStatement preparedStatement = null;
 
-        String query = "INSERT INTO Absence(numAbsence, status, FKTimeSlotPunctual, FKnumInterpreter) " +
-                "VALUES (?, ?, ?, ?)";
+        if(otherObjectID < 0) {
+            throw new IllegalArgumentException("[DAOAbsence] L'id de l'interprète ne peut pas être négatif.");
+        }
+
+        String query = "INSERT INTO Absence(, status, FKTimeSlotPunctual, FKnumInterpreter) " +
+                "VALUES ( ?, ?, ?)";
 
         try {
             preparedStatement = connect.prepareStatement(query);
 
-            preparedStatement.setInt(1, objectToInsertInDB.getNumAbsence());
-            preparedStatement.setString(2, objectToInsertInDB.getStatus());
-            preparedStatement.setInt(3, objectToInsertInDB.getTimeSlotPonctual().getNumTimeSlot());
-
-            // Problème ici : je ne sais pas comment ajouter l'id de l'interpreter si l'objet ne l'a pas de base ????
+            preparedStatement.setString(1, objectToInsertInDB.getStatus());
+            preparedStatement.setInt(2, objectToInsertInDB.getTimeSlotPunctual().getNumTimeSlot());
+            preparedStatement.setInt(3, otherObjectID);
 
             if(preparedStatement.executeUpdate() > 0) {
-                isCreated = true;
+                created = true;
             }
         } finally {
             if(preparedStatement != null) {
@@ -127,16 +147,41 @@ public class DAOAbsence extends DAO<Absence> {
             }
         }
 
-        return isCreated;
+        return created;
     }
 
-    @Override
     public boolean update(Absence objectToUpdateInDB) throws SQLException {
-        return false;
+        Connection connect = ConnectionOracle.getInstance();
+        boolean updated = false;
+        PreparedStatement preparedStatement = null;
+
+        String query = "UPDATE Absence " +
+                "SET status = ?, FKTimeSlotPunctual = ? " +
+                "WHERE numAbsence = ?";
+
+        try {
+            preparedStatement = connect.prepareStatement(query);
+            preparedStatement.setString(1, objectToUpdateInDB.getStatus());
+            preparedStatement.setInt(2, objectToUpdateInDB.getTimeSlotPunctual().getNumTimeSlot());
+            preparedStatement.setInt(3, objectToUpdateInDB.getNumAbsence());
+
+            if(preparedStatement.executeUpdate() > 0) {
+                updated = true;
+            }
+        } finally {
+            if(preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                }  catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return updated;
     }
 
-    @Override
     public boolean delete(Absence objectToDeleteFormDB) throws SQLException {
+        Connection connect = ConnectionOracle.getInstance();
         boolean isDeleted = false;
         PreparedStatement preparedStatement = null;
 
@@ -160,7 +205,6 @@ public class DAOAbsence extends DAO<Absence> {
                 }
             }
         }
-
         return isDeleted;
     }
 }
