f6d041f8f14905530e6887cd3c59aaa4a152433f
LouisHalet <louis.halet@student.hers.be>
2026-04-16 00:54:51 +0200

    feat: ajout de la classe DAOCoordinator

---
+++ src/main/java/be/hers/info/ProjetIntegree/DAO/DAOCoordinator.java
@@ -0,0 +1,245 @@
+package be.hers.info.ProjetIntegree.DAO;
+
+import be.hers.info.ProjetIntegree.POJO.*;
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.jdbc.OracleTypes;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * @author Halet Louis
+ * @reviewer Nicolas Jean-Francois
+ */
+public class DAOCoordinator extends DAO<Coordinator> {
+    /**
+     * Close the PreparedStatement passed as a parameter
+     * @param prStat the PreparedStatement to close
+     */
+    public void closeStatement(PreparedStatement prStat){
+        if(prStat != null){
+            try{
+                prStat.close();
+            }
+            catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Close the PreparedStatement and the ResultSet passed as parameters
+     * @param prStat the PreparedStatement to close
+     * @param rs the ResultSet to close
+     */
+    public void closeStatementAndResultSet(PreparedStatement prStat, ResultSet rs){
+        if(rs != null){
+            try{
+                rs.close();
+            }
+            catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+
+        closeStatement(prStat);
+    }
+
+    /**
+     * Search for a coordinator based on their coordinator number
+     * Precondition :
+     *  The Interpreter exist in the database
+     *  The appointment number passed as a parameter cannot be negative.
+     * The coordinator is initialized with the DAOInterpreter
+     * The initialized fields are: numCoordinator, isAdmin and the Interpreter with heritage
+     * @param idToSearchInDB the identifier of the object to search for in the table.
+     * @return null if the coordinator does not exist in the database, the coordinator initialized with the attributes above.
+     * @throws SQLException In case of any SQL problems encountered with this method.
+     */
+    @Override
+    public Coordinator find(int idToSearchInDB) throws SQLException {
+        PreparedStatement prStat = null;
+        ResultSet resultSet = null;
+        Coordinator coordinator = null;
+        DAOInterpreter daoInterpreter = new DAOInterpreter();
+        String query = "SELECT numCoordinator,isAdmin,FKnumInterpreter " +
+                "FROM Coordinator " +
+                "WHERE numCoordinator = ?";
+        try {
+            prStat = connect.prepareStatement(query);
+            prStat.setInt(1, idToSearchInDB);
+            resultSet = prStat.executeQuery();
+
+            if (resultSet.next()) {
+                Interpreter interpreter= daoInterpreter.find(resultSet.getInt("FKnumInterpreter"));
+                coordinator = new Coordinator(
+                        interpreter.getNumInterpreter(),
+                        interpreter.getLogin(),
+                        interpreter.getPassword(), //Password déjà hasher va etre re-hasher
+                        interpreter.getLastName(),
+                        interpreter.getFirstName(),
+                        interpreter.getEmail(),
+                        interpreter.getPhoneNumber(),
+                        interpreter.getWeeklyWorkHours(),
+                        interpreter.getAddress());
+                coordinator.setNumCoordinator(resultSet.getInt("numCoordinator"));
+                int bool = resultSet.getInt("isAdmin");
+                coordinator.setAdmin(bool!=0);
+            }
+        } finally {
+            closeStatementAndResultSet(prStat, resultSet);
+        }
+        return coordinator;
+    }
+    /**
+     * Creates a list containing all the Coordinator in the table.
+     * Precondition :
+     * For each Coordinator, the Interpreter exist in database
+     *
+     * The initialized fields are: numCoordinator, isAdmin and the Interpreter with heritage     * @return a list containing all the Appointment in the table. An empty list is returned if the table is empty.
+     * @return a list containing all the Coordinator in the table. An empty list is returned if the table is empty.
+     * @throws SQLException In case of any SQL problems encountered with this method.
+     */
+    @Override
+    public List<Coordinator> findAll() throws SQLException {
+        PreparedStatement prStat = null;
+        ResultSet resultSet = null;
+        List<Coordinator> coordinatorList = new ArrayList<>();
+        DAOInterpreter daoInterpreter = new DAOInterpreter();
+        String query = "SELECT numCoordinator,isAdmin,FKnumInterpreter " +
+                "FROM Coordinator ";
+        try {
+            prStat = connect.prepareStatement(query);
+            resultSet = prStat.executeQuery();
+            while(resultSet.next()){
+                Interpreter interpreter= daoInterpreter.find(resultSet.getInt("FKnumInterpreter"));
+                Coordinator coordinator = new Coordinator(
+                        interpreter.getNumInterpreter(),
+                        interpreter.getLogin(),
+                        interpreter.getPassword(), //Password déjà hasher va etre re-hasher
+                        interpreter.getLastName(),
+                        interpreter.getFirstName(),
+                        interpreter.getEmail(),
+                        interpreter.getPhoneNumber(),
+                        interpreter.getWeeklyWorkHours(),
+                        interpreter.getAddress());
+                coordinator.setNumCoordinator(resultSet.getInt("numCoordinator"));
+                int bool = resultSet.getInt("isAdmin");
+                coordinator.setAdmin(bool!=0);
+                coordinatorList.add(coordinator);
+            }
+        } finally {
+            closeStatementAndResultSet(prStat, resultSet);
+        }
+        return coordinatorList;
+    }
+    /**
+     * Insert the Coordinator object passed as a parameter into the database
+     * Precondition :
+     *  The Coordinator passed as a parameter cannot be null
+     *  the Interpreter exist in database
+     *  the Interpreter have their number initialized
+     * @param objectToInsertInDB the object to be inserted into the database
+     * @return true if the Coordinator is correctly inserted into the database, false otherwise
+     * @throws SQLException In case of any SQL problems encountered with this method.
+     */
+    @Override
+    public boolean create(Coordinator objectToInsertInDB) throws SQLException {
+
+        boolean isInserted = false;
+        OraclePreparedStatement prStat = null;
+        ResultSet rs = null;
+        String local = null;
+
+        String query = "INSERT INTO Coordinator (isAdmin,FKnumInterpreter) " +
+                "VALUES (?, ?) returning numCoordinator into ?";
+
+        try{
+            prStat = (OraclePreparedStatement)connect.prepareStatement(query);
+            prStat.setInt(1,(objectToInsertInDB.isAdmin() ? 1 : 0));
+            prStat.setInt(2,objectToInsertInDB.getNumInterpreter());
+            prStat.registerReturnParameter(3, OracleTypes.INTEGER);
+            int nbLinesInsert = prStat.executeUpdate();
+            rs = prStat.getReturnResultSet();
+            int id = rs.getInt(3);
+            objectToInsertInDB.setNumCoordinator(id);
+
+            if(nbLinesInsert > 0) {
+                isInserted = true;
+            }
+        }finally {
+            closeStatementAndResultSet(prStat, rs);
+        }
+
+        return isInserted;
+    }
+    /**
+     * Deletes the Coordinator whose numCoordinator matches the numCoordinator
+     * Precondition :
+     *  The Coordinator passed as a parameter cannot be null
+     *  The numCoordinator of the Coordinator passed as a parameter is initialized with its value in the database.
+     * @param objectToDeleteFormDB the object to delete from the database
+     * @return true if the Coordinator is successfully deleted, false otherwise
+     * @throws SQLException In case of any SQL problems encountered with this method.
+     */
+    @Override
+    public boolean delete(Coordinator objectToDeleteFormDB) throws SQLException {
+        boolean isDeleted = false;
+        PreparedStatement prStat = null;
+
+        String query = "DELETE FROM Coordinator " +
+                "WHERE numCoordinator = ?";
+
+        try {
+            prStat = connect.prepareStatement(query);
+            prStat.setInt(1, objectToDeleteFormDB.getNumCoordinator());
+
+            int nbLinesDelete = prStat.executeUpdate();
+            if(nbLinesDelete > 0) {
+                isDeleted = true;
+            }
+        } finally {
+            closeStatement(prStat);
+        }
+        return isDeleted;
+    }
+
+    /**
+     * Updates the isAdmin  fields in the table (not the numCoordinator and his list)
+     * Precondition :
+     *  The numCoordinator of the Coordinator passed as a parameter is initialized with its value in the database.
+     * @param objectToUpdateInDB the object to modify in the database
+     * @return true if the Coordinator is successfully modified, false otherwise
+     * @throws SQLException In case of any SQL problems encountered with this method.
+     */
+    @Override
+    public boolean update(Coordinator objectToUpdateInDB) throws SQLException {
+        boolean isUpdated = false;
+        PreparedStatement prStat = null;
+        String local = null;
+
+        String query = "UPDATE Coordinator " +
+                "SET isAdmin = ?" +
+                " WHERE numCoordinator = ?";
+
+        try {
+            prStat = connect.prepareStatement(query);
+            prStat.setInt(1, (objectToUpdateInDB.isAdmin() ? 1 : 0));
+
+
+            int nbLinesUpdate = prStat.executeUpdate();
+            if(nbLinesUpdate > 0) {
+                isUpdated = true;
+            }
+
+        } finally {
+            closeStatement(prStat);
+        }
+        return isUpdated;
+    }
+
+
+
+}
