b12d54b5311af80e0387d812e9d3f05488da4541
Chloe Wellinger <chloe.wellinger@student.hers.be>
2026-05-06 20:37:55 +0200

    refactor: renommage du fichier InterpreteController -> InterpreterController

--- src/main/java/be/hers/info/ProjetIntegree/Controller/InterpreterController.java
+++ src/main/java/be/hers/info/ProjetIntegree/Controller/InterpreterController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/interprete")
-public class InterpreteController {
+public class InterpreterController {
 
     // Temporaire
     @GetMapping("/planning")
