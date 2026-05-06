51cf4cd40689e2d2c2b9df94120ce77bc7a75458
Jean-Francois Nicolas <jf.nicolas@student.hers.be>
2026-05-06 16:37:56 +0200

    refactor: changer les get en post pour le disconnect

--- src/main/java/be/hers/info/ProjetIntegree/Controller/NavbarController.java
+++ src/main/java/be/hers/info/ProjetIntegree/Controller/NavbarController.java
@@ -2,12 +2,12 @@ package be.hers.info.ProjetIntegree.Controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NavbarController {
 
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/login";
