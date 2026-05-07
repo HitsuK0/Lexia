402baa2af35a676f6ad7d7ab38dc7a6437a7f2b7
Loïs Rosman <lois.rosman@student.hers.be>
2026-05-07 15:42:27 +0200

    feat: Ajout du squelette permettant de récupérer la liste des Appointment du bénéficiaire

--- src/main/java/be/hers/info/ProjetIntegree/Controller/InterpreterController.java
+++ src/main/java/be/hers/info/ProjetIntegree/Controller/InterpreterController.java
@@ -1,9 +1,17 @@
 package be.hers.info.ProjetIntegree.Controller;
 
+import be.hers.info.ProjetIntegree.POJO.Appointment;
+import be.hers.info.ProjetIntegree.POJO.Beneficiary;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/interprete")
@@ -16,9 +24,21 @@ public class InterpreterController {
         return "interprete/planning";
     }
 
-    // Temporaire
+    //Horaire :
+    // - Chercher tous les RDV des bénéficiaires de l'utilisateur connecté
     @GetMapping("/planning/beneficiaires")
-    public String planningBeneficiaires(Model model) {
+    public String planningBeneficiaires(@RequestParam String start, @RequestParam String end,
+                                        @SessionAttribute("BeneficiaryConnected") Beneficiary beneficiary, Model model,
+                                        HttpServletRequest request) {
+        String dateStart = start.substring(0, 10);
+        String dateEnd = end.substring(0, 10);
+
+        List<Appointment> appointmentList = List.of();
+        
+        
+        HttpSession session = request.getSession();
+        session.setAttribute("appointmentList", appointmentList);
+
         return "interprete/planning-beneficiaires";
     }
 
