c40c192951fdd79a523ad572c3735ef191ef92fc
Loïs Rosman <lois.rosman@student.hers.be>
2026-06-03 17:41:55 +0200

    doc: [CoordinatorUsersService] : ajout des spécifications des méthodes deleteUser, countBeneficiaries, countInterpreters et countResas

--- src/main/java/be/hers/info/ProjetIntegree/Services/CoordinatorUsersService.java
+++ src/main/java/be/hers/info/ProjetIntegree/Services/CoordinatorUsersService.java
@@ -7,6 +7,12 @@ import be.hers.info.ProjetIntegree.DAO.DAOInterpreter;
 import java.sql.SQLException;
 
 public class CoordinatorUsersService {
+    /**
+     * Delete a user with his login
+     * @param login the login of the user to delete
+     * @return true if the user is deleted. Otherwise, return false
+     * @throws SQLException If an SQL error occurs with this method.
+     */
     public boolean deleteUser(String login) throws SQLException {
         boolean isDeleted = false;
 
@@ -22,16 +28,31 @@ public class CoordinatorUsersService {
         return isDeleted;
     }
 
+    /**
+     * Return the number of beneficiaries in the database
+     * @return the number of beneficiaries in the database
+     * @throws SQLException If an SQL error occurs with this method.
+     */
     public int countBeneficiaries() throws SQLException {
         DAOBeneficiary daoBeneficiary = new DAOBeneficiary();
         return daoBeneficiary.countNumberBeneficiaries();
     }
 
+    /**
+     * Return the number of interpreters in the database
+     * @return the number of interpreters in the database
+     * @throws SQLException If an SQL error occurs with this method.
+     */
     public int countInterpreters() throws SQLException {
         DAOInterpreter daoInterpreter = new DAOInterpreter();
         return daoInterpreter.countNumberInterpreters();
     }
 
+    /**
+     * Return the number of resas in the database
+     * @return the number of resas in the database
+     * @throws SQLException If an SQL error occurs with this method.
+     */
     public int countResas() throws SQLException {
         DAOCoordinator daoCoordinator = new DAOCoordinator();
         return daoCoordinator.countNumberResas();
