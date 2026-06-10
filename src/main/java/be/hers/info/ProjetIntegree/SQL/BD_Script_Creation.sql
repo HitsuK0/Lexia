f6aa22e63c1b60a4ec28525503df530d673ef51a
Quentin Vanderheyden <qvanderheyden@student.hers.be>
2026-06-10 22:43:37 +0200

    Fix : retrait de la partie supp sur le fichier de creation

--- src/main/java/be/hers/info/ProjetIntegree/SQL/BD_Script_Creation.sql
+++ src/main/java/be/hers/info/ProjetIntegree/SQL/BD_Script_Creation.sql
@@ -450,24 +450,3 @@ BEGIN
 DELETE FROM Establishment WHERE FKAddress = :OLD.numAddress;
 END;
 /
--- Suppression
---DROP table Work;
---DROP table RDVInterpreter;
---DROP table Referrer;
---DROP table ProfessionalSkillInterpreter;
---DROP table RequiredProfessionalSkill;
---DROP table ProfessionalSkill;
-
---DROP table requiredAcademicSkill;
---DROP table Appointment;
---DROP table Beneficiary;
---DROP table Establishment;
---DROP table AcademicSkillInterpreter;
---DROP table AcademicSkill;
-
---DROP table Coordinator;
---DROP table Absence;
---DROP table Interpreter;
---DROP table TimeSlotPunctual;
---DROP table TimeSlotBase;
---DROP table Address;
